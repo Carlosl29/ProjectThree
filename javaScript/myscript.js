168ebc44dc87637da9adf5537ab066e8d87c388d
Carlosl29 <c.lopez2997@gmail.com>
2018-04-16 22:04:13 -0500

    Revert "Add the map"
    This reverts commit d3a2c0db78a307e470994fc7a0e729f0b0605035.

--- javaScript/myscript.js
+++ javaScript/myscript.js
@@ -1,5 +1,11 @@
 function initMap() {
-  var coord = {lat: 41.83, lng: -87.633889};
-  var map = new google.maps.Map(document.getElementById('javaScript'), {zoom: 4, center: coord});
-  var marker = new google.maps.Marker({ position: coord,map: javaScript});
+  var uluru = {lat: 41.83, lng: -87.633889};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 }
