d3a2c0db78a307e470994fc7a0e729f0b0605035
Carlosl29 <c.lopez2997@gmail.com>
2018-04-16 21:56:02 -0500

    Add the map
    Made the appropriate changes to the tags for adding the map window

--- javaScript/myscript.js
+++ javaScript/myscript.js
@@ -1,8 +1,5 @@
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-} 
+function initMap() {
+  var coord = {lat: 41.83, lng: -87.633889};
+  var map = new google.maps.Map(document.getElementById('javaScript'), {zoom: 4, center: coord});
+  var marker = new google.maps.Marker({ position: coord,map: javaScript});
+}
