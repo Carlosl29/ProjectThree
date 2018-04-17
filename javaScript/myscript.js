ac09e5de667d88e20d1b8a7659d5e15bf342dc87
Carlosl29 <c.lopez2997@gmail.com>
2018-04-17 14:59:46 -0500

    Set location
    Set the default location of the map to IIT

--- javaScript/myscript.js
+++ javaScript/myscript.js
@@ -1,11 +1,19 @@
+var map;
+var marker;
+var current;
+var iit = {lat: 41.834653, lng: -87.628333};
+    
+var places=["Chicago","Blacksburg","Huntington","Saigon"];
+      
 function initMap() {
-  var uluru = {lat: 41.83, lng: -87.633889};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+  current = {lat: 41.887855, lng: -87.630347};
+  map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 13,
+  center: iit
   });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
+
+  marker = new google.maps.Marker({
+  position: iit,
+  map: map
   });
 }
